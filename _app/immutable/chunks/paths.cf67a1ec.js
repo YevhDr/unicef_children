d647ee55cc817756fa0f2036b5b90c3d4089a086
yevhdr <evgesha.drozdova@gmail.com>
2024-09-09 14:00:59 +0300

    first commit

---
+++ _app/immutable/chunks/paths.cf67a1ec.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_le4u3c)==null?void 0:s.base)??"/unicef_children";var e;const l=((e=globalThis.__sveltekit_le4u3c)==null?void 0:e.assets)??a;export{l as a,a as b};

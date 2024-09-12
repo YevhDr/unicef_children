5646f8c23637a0be1aa65ac4d2c28c67b462de3a
yevhdr <evgesha.drozdova@gmail.com>
2024-09-12 19:57:14 +0300

    mobile fix

---
+++ _app/immutable/chunks/paths.74a3c678.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1pnzufm)==null?void 0:s.base)??"/unicef_children";var e;const t=((e=globalThis.__sveltekit_1pnzufm)==null?void 0:e.assets)??a;export{t as a,a as b};
